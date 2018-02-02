b7927f5f7c91fa46787666c88f08989fd93c1654
Heath Bieker <36055851+hbieker@users.noreply.github.com>
2018-02-01 19:00:49 -0700

    Update Web Scrape Working Copy_1.py

--- Web Scrape Working Copy_1.py
+++ Web Scrape Working Copy_1.py
@@ -2,41 +2,31 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-#Get page and verify 200 status code for response
+#Get page 
 page = requests.get('https://coinmarketcap.com/currencies/ethereum/historical-data/')
-print(page.status_code)
 
 #Create BeautifluSoup object. Case-sensitive!
 soup = BeautifulSoup(page.text, 'html.parser')
 
 #Find all td tags. Stores each table data in a list (array)
-#price_list = soup.find('tbody')
 price_list_items = soup.findAll('td')
 
-#remove_html = soup.find(class_='col-xs-12 col-md-6 text-right')
-#remove_html.decompose()
-
-#remove_menu = soup.find(class_='dropdown-menu')
-#remove_menu.decompose()
-
-#Print Ethereum Prices - includes all html tags
-#for price in price_list_items:        
-#    print (price)
-
-#for price_only in price_list_items:
-#    price = price_only.contents[0]
-#    print (price)
-
 #Create new CSV file and write column headers
-file = csv.writer(open('Monthly-Ethereum-Prices.csv', 'w'))
+file = csv.writer(open('Monthly-Ethereum-Prices.csv', 'w', newline=''))
+file.writerow(['MONTHLY ETHEREUM MARKET DATA'])
 file.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap'])
 
-#Print Ethererum Prices - data only w/no tags!
-day1 = []
-for price_only in price_list_items[0:7]:
+#Get the Ethereum data contents from the <td> tags and move them into a list
+all_stats_list = []
+for price_only in price_list_items:
     price = price_only.contents[0]
-    day1.append(price)
-    print (day1)
-file.writerow(day1)
-    
-    
+    all_stats_list.append(price)
+
+#Loop through 7 elements at a time and write the list to file
+while all_stats_list:
+    one_line_of_stats = []
+    for stats in all_stats_list[0:7]:
+        stat = all_stats_list[0]
+        one_line_of_stats.append(stat)
+        all_stats_list.pop(0)
+    file.writerow(one_line_of_stats)
